f4497df586272cfd42812e2d0dbfa12ddee10989
Pham Viet Hung <112598471+pham0084@users.noreply.github.com>
2023-02-27 03:29:37 +0800

    Update slide.py

--- slide.py
+++ slide.py
@@ -9,18 +9,25 @@ prs = Presentation()
 def generateslide(summary):
     list = summary.split('\n')
     for str in list:
-        slide = prs.slides.add_slide(prs.slide_layouts[6])
+        slide = prs.slides.add_slide(prs.slide_layouts[6])  
+        
         prs.slide_width = Inches(16)
         prs.slide_height = Inches(9)
+        
         listsentence = str.split('.')
+        
         textbox = slide.shapes.add_textbox(Inches(1), Inches(1), Inches(8), Inches(1.5))
         textframe = textbox.text_frame
+        
         listtext=['']
+        
         for sentence in listsentence:
             senend=sentence+'\n'
             listtext.append(listtext[-1]+senend)
         textframe.text = listtext[-1]
-s = ' (airst) pvhqb47@Phams-MacBook-Pro slide_rec % python slide.py\
-(airst) pvhqb47@Phams-MacBook-Pro slide_rec % rm basic_presentation.pptx\ '
+
+s = '  % python slide.py\
+(airst % rm basic_presentation.pptx\ '
+
 generateslide(s)
-prs.save('huhuhihi.pptx')
+prs.save('PDFtemplate.pptx')
